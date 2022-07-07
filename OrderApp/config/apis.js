c9695aefe3eae25b55995611469a33c71dc4e068
nguyenqhuy1512 <105408444+huynguyenmeosys@users.noreply.github.com>
2022-07-07 12:41:59 +0700

    upsource 2022-07-07 (2rd)

--- OrderApp/config/apis.js
+++ OrderApp/config/apis.js
@@ -1,27 +1,27 @@
 const REAL_URL = 'https://mresapi.herokuapp.com/api'
 const BASE_URL = 'http://192.168.1.2:5000/api'
 
-const USER_PATH = `${BASE_URL}/User`
-const MENU_PATH = `${BASE_URL}/Menu`
-const PRODUCT_PATH = `${BASE_URL}/Product`
-const TABLE_PATH = `${BASE_URL}/Table`
-const RECEIPT_PATH = `${BASE_URL}/Receipt`
-const TABLE_INFO_PATH = `${BASE_URL}/TableInfo`
-const TABLE_ORDER_PATH = `${BASE_URL}/TableOrder`
-const NOTIFICATION_SUB = `${BASE_URL}/notification/subscribe`
-const NOTIFICATION_UNSUV = `${BASE_URL}/notification/unsubscribe`
-const NOTIFICATION_SEND = `${BASE_URL}/notification/sendtotopic`
+// const USER_PATH = `${BASE_URL}/User`
+// const MENU_PATH = `${BASE_URL}/Menu`
+// const PRODUCT_PATH = `${BASE_URL}/Product`
+// const TABLE_PATH = `${BASE_URL}/Table`
+// const RECEIPT_PATH = `${BASE_URL}/Receipt`
+// const TABLE_INFO_PATH = `${BASE_URL}/TableInfo`
+// const TABLE_ORDER_PATH = `${BASE_URL}/TableOrder`
+// const NOTIFICATION_SUB = `${BASE_URL}/notification/subscribe`
+// const NOTIFICATION_UNSUV = `${BASE_URL}/notification/unsubscribe`
+// const NOTIFICATION_SEND = `${BASE_URL}/notification/sendtotopic`
 
-// const USER_PATH = `${REAL_URL}/User`
-// const MENU_PATH = `${REAL_URL}/Menu`
-// const PRODUCT_PATH = `${REAL_URL}/Product`
-// const TABLE_PATH = `${REAL_URL}/Table`
-// const RECEIPT_PATH = `${REAL_URL}/Receipt`
-// const TABLE_INFO_PATH = `${REAL_URL}/TableInfo`
-// const TABLE_ORDER_PATH = `${REAL_URL}/TableOrder`
-// const NOTIFICATION_SUB = `${REAL_URL}/notification/subscribe`
-// const NOTIFICATION_UNSUV = `${REAL_URL}/notification/unsubscribe`
-// const NOTIFICATION_SEND = `${REAL_URL}/notification/sendtotopic`
+const USER_PATH = `${REAL_URL}/User`
+const MENU_PATH = `${REAL_URL}/Menu`
+const PRODUCT_PATH = `${REAL_URL}/Product`
+const TABLE_PATH = `${REAL_URL}/Table`
+const RECEIPT_PATH = `${REAL_URL}/Receipt`
+const TABLE_INFO_PATH = `${REAL_URL}/TableInfo`
+const TABLE_ORDER_PATH = `${REAL_URL}/TableOrder`
+const NOTIFICATION_SUB = `${REAL_URL}/notification/subscribe`
+const NOTIFICATION_UNSUV = `${REAL_URL}/notification/unsubscribe`
+const NOTIFICATION_SEND = `${REAL_URL}/notification/sendtotopic`
 
 export default{
      BASE_URL,
